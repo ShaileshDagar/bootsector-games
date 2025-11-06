64cfe797d6c9013c666641bf7841dd4f5b33ee44
Shailesh Dagar <dagar.shailesh@gmail.com>
2025-11-06 13:33:52 +0530

    add player paddles and ball

--- pong/bootsector_pong.asm
+++ pong/bootsector_pong.asm
@@ -1,35 +1,87 @@
 use16
 
-org 8000h
+org 07C00h
 
-mov ax, 0003h               ; AL = 03H text mode 80x25 characters, 16 color VGA
-int 10h
+jmp setup_game
 
-;; Set up video memory
-mov ax, 0B800h
-mov es, ax
+;; CONSTANTS -----------------
+VIDMEM  equ 0B800h                          ; color text mode VGA memory location
+ROWLEN  equ 160                             ; 80 character row * 2 bytes each
+PLAYERX equ 4
+CPUX    equ 154
+
+;; Variables -----------------
+drawColor:  db 0F0h
+playerY:    dw 10
+cpuY:       dw 10
+ballX:      dw 66
+ballY:      dw 7
+
+setup_game:
+    mov ax, 0003h                           ; AL = 03H text mode 80x25 characters, 16 color VGA
+    int 10h
+
+    ;; Set up video memory
+    mov ax, VIDMEM
+    mov es, ax
 
 game_loop:
 
     ;; Clear screen to black every loop
-;    xor ax, ax
-;    xor di, di
-;    mov cx, 80*25
-;    rep stosw
-
+    xor ax, ax
     xor di, di
-    mov ax, 0F41h
-    stosw
+    mov cx, 80*25
+    rep stosw
 
-;; Draw stuff to screen
+    ;; Draw middle separating line
+    mov ah, [drawColor]                     ; White bg, black fg
+    mov di, 39*2                            ; 39th column, each column is 2 Bytes
+    mov cx, 13                              ; Drawing every other row
+    .draw_middle_loop:
+        stosw
+        add di, 2*ROWLEN - 2                ; Only drawing every other row, subtracting 2 to because loop increments
+        loop .draw_middle_loop
 
-;; Player Input
+    ;; Draw Player Paddle
+    imul di, [playerY], ROWLEN              ; Y position is Y (# of rows) * length or row
+    add di, PLAYERX
+    mov cl, 5
+    .draw_player_loop:
+        stosw
+        add di, ROWLEN - 2
+        loop .draw_player_loop
 
-;; CPU Input
+    ;; Draw CPU Paddle
+    imul di, [cpuY], ROWLEN
+    mov cl, 5
+    .draw_cpu_paddle:
+        mov [es:di+CPUX], ax
+        add di, ROWLEN
+        loop .draw_cpu_paddle
+
+    ;; Draw Ball
+    imul di, [ballY], ROWLEN
+    add di, [ballX]
+    mov word [es:di], 2000h
+    
+    ;; Get Player Input
+
+    ;; Move CPU
+
+    ;; Move Ball
+
+    ;; Delay loop
+    mov bx, [046Ch]
+    inc bx
+    inc bx
+    .delay:
+        cmp [046Ch], bx
+        jl .delay
+
 
 jmp game_loop
 
 ;; Win/Lose condition
 
-times 510 -($-$$) db 0      ;; Bootsector padding
-dw 0AA55h                   ; Bootsector Signature
+times 510 -($-$$) db 0                      ; Bootsector padding
+dw 0AA55h                                   ; Bootsector Signature
